e7a8710b1e6b6576097b79c94b089efc87646d22
Reuben Peeris <reuben@reubenpeeris.com>
2013-12-01 13:55:46 +0000

    Revert "test"
    This reverts commit 1f85096f013b1a50da30d931970e31e5033e74f2.

--- wippen-engine/src/main/lombok/VictimClass.java
+++
@@ -1,9 +0,0 @@
-public final class VictimClass  {
-	/**
-	 * Private constructor that should 
-	 * not be invoked
-	 */
-	private VictimClass()  {
-	   System.out.println(" Instance created");
-	}
-}
--- wippen-engine/src/main/lombok/com/reubenpeeris/wippen/robotloader/Loader.java
+++
@@ -1,6 +0,0 @@
-package com.reubenpeeris.wippen.robotloader;
-
-public interface Loader<T> {
-	boolean acceptsURL(String url) throws LoaderException;
-	T createInstance(String url) throws LoaderException;
-}
